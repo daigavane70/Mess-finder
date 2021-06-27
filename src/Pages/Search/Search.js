1aecb365116e16cd758a014aa79f2480a376e3d2
daigavane70 <daigavane70@gmail.com>
2021-06-27 21:06:34 +0530

    search bar added

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,30 +1,39 @@
 import React from "react";
 import Card from "../../Components/Card";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./Search.css";
 import { defaultList } from "../../Components/data";
 import Filter from "../../Components/Filter";
+import {useState} from 'react'
 
 const ListOptions = () => {
+
+  const [filters, setFilters] = useState([
+    {type: 'veg'},
+  ]);
+
   let cards = defaultList.map((eve) => {
     return <Card details={eve}></Card>;
   });
 
   return (
     <div className="container-fluid text-center options">
-      <h1>Search Results</h1>
+      <div className="container">
+        <div className="search-bar-container row">
+          <i class="fas fa-search col-1" onClick={() => document.getElementById('search-bar').focus()} />
+          <input className="col-11" placeholder="Search" id='search-bar'/>
+        </div>
+      </div>
 
       <div className="row">
         {/* for Filters */}
-        <div className="col-lg-4 col-md-4">
+        <div className="col-lg-3 col-md-4">
           <hr />
           <h3>Filters</h3>
           <hr />
           <Filter className="filters" />
         </div>
-
         {/* for Cards */}
-        <div className="col-lg-8 text-center">
+        <div className="col-lg-9 text-center">
           <hr />
           <h3> Cards </h3>
           <hr />
