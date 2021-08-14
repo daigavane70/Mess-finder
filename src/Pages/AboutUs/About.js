f8ebcf7d527fc39af90d03008ca038136f57d1ca
Amruta-26 <darakamruta@gmail.com>
2021-08-14 11:13:35 +0530

    Modified first time

--- src/Pages/AboutUs/About.js
+++ src/Pages/AboutUs/About.js
@@ -1,9 +1,326 @@
-import React from 'react'; 
+// import React from 'react'; 
 
-const AboutUs = () => { 
-  return (
-      <h1>AboutUs</h1>
-  ); 
-}; 
+// const AboutUs = () => { 
+//   return (
+//       <h1>AboutUs</h1>
+//   ); 
+// }; 
   
+// export default AboutUs;
+
+import React from "react";
+// import "./About.css";
+
+const AboutUs = () => {
+  return (
+    <div className="AboutUs">
+        <h1 className="jumbotron text-center orange">About us</h1>
+
+
+      {/* <!-- Container (About Section) --> */}
+      <div id="about" className="container-fluid">
+        <div className="row">
+          <div className="col-sm-8">
+            <h2>Our Website</h2>
+            <br />
+            <h4>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </h4>
+            <br />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore mt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat.
+            </p>
+            <br />
+            <button className="btn btn-default btn-lg">Get in Touch</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="container-fluid bg-grey">
+        <div className="row">
+          <div className="col-sm-4">
+            <span className="glyphicon glyphicon-globe logo slideanim"></span>
+          </div>
+          <div className="col-sm-8">
+            <h2>Our Values</h2>
+            <br />
+            <p>
+              <strong>VISION:</strong> Our vision Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+               aliqua. Ut enim ad minim veniam, quis  nostrud exercita xercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div id="services" className="container-fluid text-center">
+        <h2>SERVICES</h2>
+        <h4>What we offer</h4>
+        <br />
+        <div className="row slideanim">
+          <div className="col-sm-4">
+            <span className="glyphicon glyphicon-off logo-small"></span>
+            <h4>SECURITY</h4>
+            <p>Lorem ipsum dolor sit amet..</p>
+          </div>
+          <div className="col-sm-4">
+            <span className="glyphicon glyphicon-heart logo-small"></span>
+            <h4>COMPARISONS</h4>
+            <p>Lorem ipsum dolor sit amet..</p>
+          </div>
+          <div className="col-sm-4">
+            <span className="glyphicon glyphicon-lock logo-small"></span>
+            <h4>TRACKER</h4>
+            <p>Lorem ipsum dolor sit amet..</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-light py-5">
+        <div className="container py-5">
+          <div className="row mb-4">
+            <div className="col-lg-5">
+              <h2 className="display-4 font-weight-light">Our team</h2>
+              <p className="font-italic text-muted">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>
+            </div>
+          </div>
+
+          <div className="row text-center">
+            <div className="col-xl-3 col-sm-6 mb-5">
+              <div className="bg-white rounded shadow-sm py-5 px-4">
+                <img
+                  src="https://res.cloudinary.com/mhmd/image/upload/v1556834132/avatar-4_ozhrib.png"
+                  alt=""
+                  width="100"
+                  className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
+                />
+                <h5 className="mb-0">Bhavana </h5>
+                <span className="small text-uppercase text-muted">
+                  CEO - Founder
+                </span>
+                <ul className="social mb-0 list-inline mt-3">
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-facebook-f"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-twitter"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-instagram"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-linkedin"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="col-xl-3 col-sm-6 mb-5">
+              <div className="bg-white rounded shadow-sm py-5 px-4">
+                <img
+                  src="https://res.cloudinary.com/mhmd/image/upload/v1556834132/avatar-4_ozhrib.png"
+                  alt=""
+                  width="100"
+                  className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
+                />
+                <h5 className="mb-0">Bindi </h5>
+                <span className="small text-uppercase text-muted">
+                  CEO - Founder
+                </span>
+                <ul className="social mb-0 list-inline mt-3">
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-facebook-f"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-twitter"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-instagram"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-linkedin"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="col-xl-3 col-sm-6 mb-5">
+              <div className="bg-white rounded shadow-sm py-5 px-4">
+                <img
+                  src="https://res.cloudinary.com/mhmd/image/upload/v1556834133/avatar-2_f8dowd.png"
+                  alt=""
+                  width="100"
+                  className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
+                />
+                <h5 className="mb-0">Vedant</h5>
+                <span className="small text-uppercase text-muted">
+                  CEO - Founder
+                </span>
+                <ul className="social mb-0 list-inline mt-3">
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-facebook-f"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-twitter"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-instagram"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-linkedin"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-xl-3 col-sm-6 mb-5">
+              <div className="bg-white rounded shadow-sm py-5 px-4">
+                <img
+                  src="https://res.cloudinary.com/mhmd/image/upload/v1556834133/avatar-2_f8dowd.png"
+                  alt=""
+                  width="100"
+                  className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
+                />
+                <h5 className="mb-0">Shivendra</h5>
+                <span className="small text-uppercase text-muted">
+                  CEO - Founder
+                </span>
+                <ul className="social mb-0 list-inline mt-3">
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-facebook-f"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-twitter"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-instagram"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-linkedin"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="col-xl-3 col-sm-6 mb-5">
+              <div className="bg-white rounded shadow-sm py-5 px-4">
+                <img
+                  src="https://res.cloudinary.com/mhmd/image/upload/v1556834133/avatar-1_s02nlg.png"
+                  alt=""
+                  width="100"
+                  className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
+                />
+                <h5 className="mb-0">Karan</h5>
+                <span className="small text-uppercase text-muted">
+                  CEO - Founder
+                </span>
+                <ul className="social mb-0 list-inline mt-3">
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-facebook-f"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-twitter"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-instagram"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-linkedin"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            {/* <!-- Team item--> */}
+            <div className="col-xl-3 col-sm-6 mb-5">
+              <div className="bg-white rounded shadow-sm py-5 px-4">
+                <img
+                  src="https://res.cloudinary.com/mhmd/image/upload/v1556834132/avatar-4_ozhrib.png"
+                  alt=""
+                  width="100"
+                  className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
+                />
+                <h5 className="mb-0">Amruta </h5>
+                <span className="small text-uppercase text-muted">
+                  CEO - Founder
+                </span>
+                <ul className="social mb-0 list-inline mt-3">
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-facebook-f"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-twitter"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-instagram"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-linkedin"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default AboutUs;
--- src/Pages/ContactUs/ContactUs.js
+++ src/Pages/ContactUs/ContactUs.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 // import {
 //   MDBRow,
 //   MDBCol,
@@ -9,9 +8,6 @@ import React from "react";
 //   MDBBtn,
 //   MDBInput,
 // } from "mdbreact";
-=======
-import { MDBRow, MDBContainer, MDBCol, MDBCard, MDBCardBody, MDBIcon, MDBBtn, MDBInput } from "mdbreact";
->>>>>>> origin/main
 
 const ContactPage = () => {
   return (
@@ -20,20 +16,19 @@ const ContactPage = () => {
         Contact us
       </h2>
       <p className="text-center w-responsive mx-auto pb-5">
-<<<<<<< HEAD
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error
         amet numquam iure provident voluptate esse quasi, veritatis totam
         voluptas nostrum quisquam eum porro a pariatur veniam.
       </p>
       <div className="row">
-        <div className="col-lg-5">
+        <div className="col-lg-6" style={{ margin: 25 }}>
           <div className="card">
             <div className="card-body">
               <div className="form-header blue accent-1">
                 <h3 className="mt-2 text-center">
                   <i class="fas fa-envelope"></i> Write to us:
                 </h3>
-              </div> 
+              </div>
               <p className="dark-grey-text">
                 We'll write rarely, but only the best content.
               </p>
@@ -107,73 +102,12 @@ const ContactPage = () => {
             </div>
           </div>
         </div>
-        
-        <div className="col-lg-7">
-=======
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
-        error amet numquam iure provident voluptate esse quasi, veritatis
-        totam voluptas nostrum quisquam eum porro a pariatur veniam.
-      </p>
-      {/* <MDBRow>
-        <MDBCol lg="5" className="lg-0 mb-4">
-          <MDBCard>
-            <MDBCardBody>
-              <div className="form-header blue accent-1">
-                <h3 className="mt-2">
-                  <MDBIcon icon="envelope" /> Write to us:
-                </h3>
-              </div>
-              <p className="dark-grey-text">
-                We'll write rarely, but only the best content.
-              </p>
-              <div className="md-form">
-                <MDBInput
-                  icon="user"
-                  label="Your name"
-                  iconClass="grey-text"
-                  type="text"
-                  id="form-name"
-                />
-              </div>
-              <div className="md-form">
-                <MDBInput
-                  icon="envelope"
-                  label="Your email"
-                  iconClass="grey-text"
-                  type="text"
-                  id="form-email"
-                />
-              </div>
-              <div className="md-form">
-                <MDBInput
-                  icon="tag"
-                  label="Subject"
-                  iconClass="grey-text"
-                  type="text"
-                  id="form-subject"
-                />
-              </div>
-              <div className="md-form">
-                <MDBInput
-                  icon="pencil-alt"
-                  label="Icon Prefix"
-                  iconClass="grey-text"
-                  type="textarea"
-                  id="form-text"
-                />
-              </div>
-              <div className="text-center">
-                <MDBBtn color="light-blue">Submit</MDBBtn>
-              </div>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-        <MDBCol lg="7">
->>>>>>> origin/main
+
+        <div className="col-lg-5" style={{ margin: 25 }}>
           <div
             id="map-container"
             className="rounded z-depth-1-half map-container"
-            style={{ height: "400px" }}
+            style={{ height: "410px" }}
           >
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d76765.98321148289!2d-73.96694563267306!3d40.751663750099084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spl!2spl!4v1525939514494"
@@ -185,171 +119,70 @@ const ContactPage = () => {
             />
           </div>
           <br />
-<<<<<<< HEAD
-
-          <div className="row text-center">
-
-            <div className="col-md-4">
-            <button type="button" class="btn btn-primary">
-            <i class="fas fa-map-marker-alt fa-3x"></i>
-            </button>
-            <p>New York, 94126</p>
-            <p className="mb-md-0">United States</p>
-            </div>
-
-            <div className="col-md-4">
-            <button type="button" class="btn btn-primary">
-            <i class="fas fa-phone fa-3x"></i>
-            </button>
-            <p>+ 01 234 567 89</p>
-            <p className="mb-md-0">Mon - Fri, 8:00-22:00</p>
-            </div>
-
-            <div className="col-md-4">
-            <button type="button" class="btn btn-primary">
-            <i class="fas fa-envelope fa-3x"></i>
-            </button>
-            <p>info@gmail.com</p>
-            <p className="mb-md-0">sale@gmail.com</p>
-            </div>
-
-          </div>
-
         </div>
-
       </div>
 
+      <div className="row text-center ">
+        <div className="col-md-2">
+          <button type="button" class="btn btn-primary">
+            <i class="fas fa-map-marker-alt fa-3x"></i>
+          </button>
+          <p>New York, 94126</p>
+          <p className="mb-md-0">United States</p>
+        </div>
+
+        <div className="col-md-2">
+          <button type="button" class="btn btn-primary">
+            <i class="fas fa-phone fa-3x"></i>
+          </button>
+          <p>+ 01 234 567 89</p>
+          <p className="mb-md-0">Mon - Fri, 8:00-22:00</p>
+        </div>
+
+        <div className="col-md-2">
+        <a href="mailto:info@gmail.com" target="_blank" rel="noreferrer">
+          <button type="button" class="btn btn-primary">
+            <i class="fas fa-envelope fa-3x"></i>
+          </button>
+          </a>
+          <p>info@gmail.com</p>
+          <p className="mb-md-0">sale@gmail.com</p>
+        </div>
+
+        <div className="col-md-2">
+          <a href="whatsapp://send?text=Hello MessFinder!&amp;phone=+917006000025"
+            target="_blank" rel="noreferrer">
+            <button type="button" class="btn btn-primary">
+              <i class="fab fa-whatsapp fa-3x"></i>
+            </button>
+          </a>
+          <p>Mess Finder</p>
+          <p className="mb-md-0">Join with us on Whatsapp</p>
+        </div>
+
+        <div className="col-md-2">
+        <a href="https://twitter.com/MessFinder" target="_blank" rel="noreferrer">
+          <button type="button" class="btn btn-primary">
+            <i class="fab fa-twitter fa-3x"></i>
+          </button>
+          </a>
+          <p>@MessFinder</p>
+          <p className="mb-md-0">Join with us on Twitter</p>
+        </div>
+
+        <div className="col-md-2">
+        <a href="https://instagram.com/MessFinder/" target="_blank" rel="noreferrer">
+          <button type="button" class="btn btn-primary">
+            <i class="fab fa-instagram fa-3x"></i>
+          </button>
+          </a>
+          <p>MessFinder</p>
+          <p className="mb-md-0">Join with us on </p>
+        </div>
+      </div>
     </div>
+
   );
 };
 
 export default ContactPage;
-
-/* 
-<MDBRow>
-  <MDBCol lg="5" className="lg-0 mb-4">
-    <MDBCard>
-      <MDBCardBody>
-        <div className="form-header blue accent-1">
-          <h3 className="mt-2">
-            <MDBIcon icon="envelope" /> Write to us:
-          </h3>
-        </div>
-        <p className="dark-grey-text">
-          We'll write rarely, but only the best content.
-        </p>
-        <div className="md-form">
-          <MDBInput
-            icon="user"
-            label="Your name"
-            iconClass="grey-text"
-            type="text"
-            id="form-name"
-          />
-        </div>
-        <div className="md-form">
-          <MDBInput
-            icon="envelope"
-            label="Your email"
-            iconClass="grey-text"
-            type="text"
-            id="form-email"
-          />
-        </div>
-        <div className="md-form">
-          <MDBInput
-            icon="tag"
-            label="Subject"
-            iconClass="grey-text"
-            type="text"
-            id="form-subject"
-          />
-        </div>
-        <div className="md-form">
-          <MDBInput
-            icon="pencil-alt"
-            label="Icon Prefix"
-            iconClass="grey-text"
-            type="textarea"
-            id="form-text"
-          />
-        </div>
-        <div className="text-center">
-          <MDBBtn color="light-blue">Submit</MDBBtn>
-        </div>
-      </MDBCardBody>
-    </MDBCard>
-  </MDBCol>
-  <MDBCol lg="7">
-    <div
-      id="map-container"
-      className="rounded z-depth-1-half map-container"
-      style={{ height: "400px" }}
-    >
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d76765.98321148289!2d-73.96694563267306!3d40.751663750099084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spl!2spl!4v1525939514494"
-        title="This is a unique title"
-        width="100%"
-        height="100%"
-        frameBorder="0"
-        style={{ border: 0 }}
-      />
-    </div>
-    <br />
-    <MDBRow className="text-center">
-      <MDBCol md="4">
-        <MDBBtn tag="a" floating color="blue" className="accent-1">
-          <MDBIcon icon="map-marker-alt" />
-        </MDBBtn>
-        <p>New York, 94126</p>
-        <p className="mb-md-0">United States</p>
-      </MDBCol>
-      <MDBCol md="4">
-        <MDBBtn tag="a" floating color="blue" className="accent-1">
-          <MDBIcon icon="phone" />
-        </MDBBtn>
-        <p>+ 01 234 567 89</p>
-        <p className="mb-md-0">Mon - Fri, 8:00-22:00</p>
-      </MDBCol>
-      <MDBCol md="4">
-        <MDBBtn tag="a" floating color="blue" className="accent-1">
-          <MDBIcon icon="envelope" />
-        </MDBBtn>
-        <p>info@gmail.com</p>
-        <p className="mb-md-0">sale@gmail.com</p>
-      </MDBCol>
-    </MDBRow>
-  </MDBCol>
-</MDBRow> */
-=======
-          <MDBRow className="text-center">
-            <MDBCol md="4">
-              <MDBBtn tag="a" floating color="blue" className="accent-1">
-                <MDBIcon icon="map-marker-alt" />
-              </MDBBtn>
-              <p>New York, 94126</p>
-              <p className="mb-md-0">United States</p>
-            </MDBCol>
-            <MDBCol md="4">
-              <MDBBtn tag="a" floating color="blue" className="accent-1">
-                <MDBIcon icon="phone" />
-              </MDBBtn>
-              <p>+ 01 234 567 89</p>
-              <p className="mb-md-0">Mon - Fri, 8:00-22:00</p>
-            </MDBCol>
-            <MDBCol md="4">
-              <MDBBtn tag="a" floating color="blue" className="accent-1">
-                <MDBIcon icon="envelope" />
-              </MDBBtn>
-              <p>info@gmail.com</p>
-              <p className="mb-md-0">sale@gmail.com</p>
-            </MDBCol>
-          </MDBRow>
-        </MDBCol>
-      </MDBRow> */}
-    </div>
-  );
-}
-
-export default ContactPage;
->>>>>>> origin/main
